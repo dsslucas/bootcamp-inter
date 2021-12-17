216072058e5fc968a1ec00920c757e387fc6e04e
Lucas Silva <lucas_silva_souza@hotmail.com>
2021-12-17 18:17:23 -0300

    Última atualização no exercício de condições JavaScript

--- introducao-javascript/web-js/assets/js/scripts.js
+++ introducao-javascript/web-js/assets/js/scripts.js
@@ -1,21 +1,32 @@
 console.log("Hello World")
 
 //ID do SPAN
-var buttonIncrement = document.getElementsByName("adicionar")
+var buttonDecrement = document.getElementsByName("subtrair")[0]
+var buttonIncrement = document.getElementsByName("adicionar")[0]
 var currentNumberWrapper = document.getElementById("currentNumber");
 var currentNumber = 0;
 
 function condiction(){
+    console.log("Número atual:", currentNumber)
+
     if(currentNumber < 0){
         currentNumberWrapper.style.color = "red"
+        if(currentNumber === -10){
+            buttonDecrement.disabled = true;
+        }
     }
-    /*
-    else if( currentNumber >= 10){
-        console.log("Numero", currentNumber);
-        //buttonIncrement.style.display = "none";
-        buttonIncrement.style.background = "pink";
-    }
-    */
+    else{
+        currentNumberWrapper.style.color = "black"
+        if(currentNumber === 10){
+            buttonIncrement.disabled = true;
+        }
+
+        else{
+            buttonIncrement.disabled = false;
+            buttonDecrement.disabled = false; 
+        }
+    }  
+    
     currentNumberWrapper.innerHTML = currentNumber
 }
 
